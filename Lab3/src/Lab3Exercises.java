8deaec827fdba0ad20403cff7eca427f406ec2aa
4015a2 <4015a2@mandarina.ece.uprm.edu>
2017-11-15 12:20:02 -0400

    Personalized the template code for Lab3.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -1,11 +1,11 @@
 import javax.swing.JFrame;
 
 public class Lab3Exercises {
-	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
-		myFrame.setVisible(true);
-	}
+    public static void main(String[] args) {
+        JFrame myFrame = new JFrame("Angelic Arzola");
+        myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        myFrame.setLocation(800, 350);
+        myFrame.setSize(200, 200);
+        myFrame.setVisible(true);
+    }
 }
