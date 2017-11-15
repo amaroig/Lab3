747b9d0aa537862b1fd2abd9ea30ef640f5d114f
4015a2 <4015a2@mandarina.ece.uprm.edu>
2017-11-15 13:10:56 -0400

    Added polygons.

--- Lab3/src/MyPanelClass.java
+++ Lab3/src/MyPanelClass.java
@@ -1,6 +1,8 @@
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Insets;
+import java.awt.Polygon;
+
 import javax.swing.JPanel;
  
 public class MyPanelClass extends JPanel {
@@ -24,21 +26,52 @@ public class MyPanelClass extends JPanel {
                         //Paint the background
                         g.setColor(Color.BLACK);
                         g.fillRect(x1, y1, width + 1, height + 1);
+                        
                         //Draw a border
                         g.setColor(Color.RED);
                         g.drawRect(x1, y1, width, height);
                         g.setColor(Color.YELLOW);
                         g.drawRect(x1+2, y1+2, width-5, height-5);
-//                        //Diagonal Line L->R
-//                        g.setColor(Color.WHITE);
-//                        g.drawLine(x1, y1, x2, y2);
-//                        //Diagonal Line R->L
-//                        g.setColor(Color.CYAN);
-//                        g.drawLine(0, y2, x2, 0);
-                        int ovalCenter1 = (width-55)/2;
-                        int ovalCenter2 = (height-55)/2;
-                        g.setColor(Color.LIGHT_GRAY);
-                        g.fillOval(ovalCenter1, ovalCenter2, 55, 55);
                         
+//                      //Diagonal Line L->R
+//                      g.setColor(Color.WHITE);
+//                      g.drawLine(x1, y1, x2, y2);
+                        
+//                      //Diagonal Line R->L
+//                      g.setColor(Color.CYAN);
+//                      g.drawLine(0, y2, x2, 0);
+                        
+//                      //Draw Oval
+//                      int ovalCenter1 = (width-55)/2;
+//                      int ovalCenter2 = (height-55)/2;
+//                      g.setColor(Color.LIGHT_GRAY);
+//                      g.fillOval(ovalCenter1, ovalCenter2, 55, 55);
+                        
+//                      //Draw Polygon
+//                      Polygon p = new Polygon();
+//                      p.addPoint(x1 + 5, y1 + 25);
+//                      p.addPoint(x1 + 20, y1 + 10);
+//                      p.addPoint(x1 + 35, y1 + 25);
+//                      p.addPoint(x1 + 25, y1 + 25);
+//                      p.addPoint(x1 + 25, y1 + 45);
+//                      p.addPoint(x1 + 15, y1 + 45);
+//                      p.addPoint(x1 + 15, y1 + 25);
+//                      g.setColor(Color.YELLOW);
+//                      g.fillPolygon(p);
+                        
+                        //Draw Polygon2
+                        Polygon p2 = new Polygon();
+                        p2.addPoint(x1 + 25, y1 + 73);
+                        p2.addPoint(x1 + 41, y1 + 73);
+                        p2.addPoint(x1 + 47, y1 + 58);
+                        p2.addPoint(x1 + 53, y1 + 73);
+                        p2.addPoint(x1 + 69, y1 + 73);
+                        p2.addPoint(x1 + 56, y1 + 83);
+                        p2.addPoint(x1 + 61, y1 + 98);
+                        p2.addPoint(x1 + 47, y1 + 88);
+                        p2.addPoint(x1 + 34, y1 + 98);
+                        p2.addPoint(x1 + 38, y1 + 83);
+                        g.setColor(Color.WHITE);
+                        g.drawPolygon(p2);
             }
 }
